bf54a8b272854f7c308982722dbc8e993fac376a
AuRiv <aureliaviglione@gmail.com>
2026-04-21 14:56:46 +0200

    Add files via upload

---
+++ Tutorial 2/Tutorial 2 - Cross-sectional graph.R
@@ -0,0 +1,162 @@
+#---------------------------------Load libraries-------------------------------#
+library("qgraph") 
+library("bootnet") 
+library("NetworkComparisonTest")
+library("ggplot2")
+library("readxl")
+library("dplyr")
+
+#---------------------------------Load files-------------------------------#
+setwd()
+data <- read_excel(".xlsx")
+
+
+nodes=grep("item", names(data), value = TRUE)
+data <- data %>%
+  mutate_at(vars(nodes), as.numeric)
+
+
+
+#--------------------Regularized network (bootnet estimation)-------------------------------#
+#to limit spurious edges
+
+#---------------------------------Set regularization parameters--------------------------#
+#gamma: control how much sparse is the model, set between 0 and 0.5, higher values more parsimonious model
+t <- 0.5 
+
+
+control <- subset(data, data$Arm == "Control")
+network1 <- estimateNetwork(
+  data = control[, nodes],
+  default = "EBICglasso",
+  corMethod = "cor",
+  corArgs = list(method = "spearman"),
+  tuning = t,              # gamma
+  lambda.min.ratio = 0.01,   # minimum lambda
+  missing = "pairwise"
+)
+
+
+
+#------- Small summary of estimated network-------------------------------#
+network1
+network1$graph
+cs=sum(abs(network1$graph))/2 #connectivity strenght 
+
+#--Plot estimated netwotk---#
+node_colors <- grDevices::hcl.colors(length(nodes), "Dark 3")
+groups <- as.list(nodes)
+names(groups) <- nodes #Each node becomes its own group so the legend shows its color
+
+plot(network1,
+     layout = "spring",
+     
+     # Node fill color
+     color = node_colors,
+     vsize = 7,
+     
+     # Label settings
+     label.cex = 0.7,
+     label.color = "black",
+     label.prop = 0.9,
+     labels = FALSE,
+     
+     # Edges
+     negDashed = FALSE,
+     
+     groups = nodes,
+     
+     # Legend
+     legend.cex = 0.50,
+     legend.mode = "style2",
+     nodeNames = nodes,
+    
+     
+     # General
+     font = 2
+)
+
+
+
+#--------------------------Stability of edge weights------------------------------------#
+#non-parametric bootstrap: sampling cases randomly with replacement
+
+#Are our edge weights? How much do they change as we change our sample? 
+bootnet_edge_weights <- bootnet(network1, 
+                          nBoots = 1000, # number of boot samples
+                          nCores = 8)
+
+# Looking at the width of variation
+plot(bootnet_edge_weights,
+     labels = TRUE,
+     order = "sample")
+
+
+
+#---Stability of centrality measures---#
+#case-dropping bootstrap: randomly exclude cases from our original dataset, creating a new dataset
+
+centralityPlot(network1, include = "all", orderBy = "ExpectedInfluence")
+
+
+#Are our centrality indices realiable? How much do they change as we change our sample? 
+bootnet_centrality <- bootnet(network1, 
+                                 nBoots = 1000,
+                                 type = "case",
+                                 nCores = 8,
+                                 statistics = c('strength',
+                                                'expectedInfluence',
+                                                'betweenness',
+                                                'closeness'))
+
+plot(bootnet_centrality, 'all')
+
+
+#Correlation-Stability Coefficient (CS-Coefficient)
+corStability(bootnet_case_dropping)
+
+
+#----------------------------------------------------Network comparison-------------------------------------------------------#
+treated <- subset(data, data$Arm == "Treatment")
+network2 <- estimateNetwork(
+  data = treated [, nodes],
+  default = "EBICglasso",
+  corMethod = "cor",
+  corArgs = list(method = "spearman"),
+  tuning = t,              # gamma
+  lambda.min.ratio = lmr,   # minimum lambda
+  missing = "pairwise"
+)
+
+
+ntc <- NCT(network1, network2, it=1000, binary.data=FALSE, test.edges=TRUE, edges='all', progressbar=TRUE)
+
+
+#-------------------Global strength comparison------------------------#
+ntc$glstrinv.sep   #connectivity value 
+ntc$glstrinv.real  #difference in connectivity
+ntc$glstrinv.pval  #p-value
+
+
+#-----------------No regularized network------------------#
+network3=estimateNetwork(control[, nodes], default = "pcor", corMethod = "cor", corArgs = list(method = "spearman"))
+
+
+#--------------------------Exercise--------------------#
+#1) How does changing the gamma parameter affect 'control'?
+#Test gamma values of 0.25 and 0.10
+#plot the graphs.
+#examine how connectivity strength changes.
+#compare edge weight stability 
+
+#2) Compare global connectivity strength between smokers and non-smokers.
+
+
+#3) Which is the most central node for subjects treated with SSRI
+
+
+
+
+
+
+
